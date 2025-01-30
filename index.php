90395a06af30ad094730819a8acf4528619a83e8
AndreyMonti <andreymontibeller02@gmail.com>
2025-01-30 09:56:52 -0300

    pasta_img

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zen Fitness</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <script src="./javascript/index.js"></script>
+</head>
+<body>
+    <!-- Header principal -->
+
+    <header>
+
+
+        <a href="#home" class="logo">Zen <span>Fitness</span></a>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#sobre">Sobre nós</a></li>
+            <li><a href="aulas.php"></a>Agendar aulas</li>
+        </ul>
+
+
+    </header>
+</body>
+</html>
