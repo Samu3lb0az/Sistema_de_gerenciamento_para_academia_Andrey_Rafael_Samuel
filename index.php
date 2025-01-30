d2762991dc8fbec4182b35dfdd97daf3b54c22cb
AndreyMonti <andreymontibeller02@gmail.com>
2025-01-30 12:07:08 -0300

    sobre-section

--- index.php
+++ index.php
@@ -29,5 +29,47 @@
 
     </header>
 
+
+    <!-- Section da página home -->
+     <section class="home" id="home">
+        <div class="home-content">
+            <h3>Construa o seu</h3>
+            <h1>Físico dos sonhos</h1>
+            <h3><span class="multiple-text">Bodybuilding</span></h3>
+
+            <p>Conheça mais sobre nossa rede de academias!</p>
+
+            <a href="#sobre" class="btn">Saiba mais</a>
+
+            
+        </div>
+
+        <div class="home-img">
+            <img src="./img/home_background.png" alt="Background">
+        </div>
+        
+     </section>
+
+    <!-- Section da página sobre -->
+     
+    <section class="sobre" id="sobre">
+        <div class="sobre-img">
+            <img src="./img/sobre-img.jpg" alt="Homem correndo na esteira">
+        </div>
+
+        <div class="sobre-content">
+            <h2 class="heading">Por que nos escolher?</h2>
+
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+            <p>But I must explain to you how all this mistaken idea of denouncing pleasure.</p>
+
+            <a href="cadastro.php" class="btn">Matricule-se agora!</a>
+
+        </div>
+         
+    </section>
+
 </body>
 </html>
