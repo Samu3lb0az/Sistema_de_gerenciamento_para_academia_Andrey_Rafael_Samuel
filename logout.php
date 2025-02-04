a55994d706a4cb80dc03385a19c3423971618c49
AndreyMonti <andreymontibeller02@gmail.com>
2025-02-04 09:04:20 -0300

    correcao-logout

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
+// Inicia a sessão
 session_start();
+
+// Remove todas as variáveis de sessão
+session_unset();
+
+// Destrói a sessão
 session_destroy();
+
+// Redireciona para a página inicial ou de login
 header("Location: index.php");
-exit();
+exit;
 ?>
